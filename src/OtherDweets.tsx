f3961b44ec3139962c5e0e65ad4b80a3ca9d9dcf
andredomin <108339769+andredomin@users.noreply.github.com>
2024-01-17 17:49:58 +0100

    Fixing errors [3]

--- src/OtherDweets.tsx
+++ src/OtherDweets.tsx
@@ -54,8 +54,8 @@ const OtherDweets: React.FC = () => {
   return (
     <>
     <Dweets
-      pathImage='../public/img/toools.jpeg'
-      pathImage2='../public/img/profile-pic.jpeg'
+      pathImage='/img/toools.jpeg'
+      pathImage2='/img/profile-pic.jpeg'
       date='16 Jan 24'
       dweetTitle='A stunning resource for designers and programmers.'
       tags={tagsresource}
@@ -63,8 +63,8 @@ const OtherDweets: React.FC = () => {
       btnprop={btnprop2}
     />
     <Dweets
-      pathImage="../public/img/antibiotic.jpeg"
-      pathImage2="../public/img/profile-pic.jpeg"
+      pathImage="/img/antibiotic.jpeg"
+      pathImage2="/img/profile-pic.jpeg"
       date="16 Jan 24"
       dweetTitle="AI discovers new class of antibiotics to kill drug-resistant bacteria"
       tags={tagsantibiotic}
